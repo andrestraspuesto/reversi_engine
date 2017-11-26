729bc9dc3b6025f235f1f1ec2d121c5fc01e06a4
andrestraspuesto <andrestraspuesto@gmail.com>
2017-11-26 19:37:51 +0100

    añade config de entorno de desarrollo a git

---
+++ config/dev.exs
@@ -0,0 +1,41 @@
+use Mix.Config
+
+config :reversi_engine, :board_1, value: {
+  {nil, nil, nil, nil},
+  {nil, "B", "W", nil},
+  {nil, "W", "B", nil},
+  {nil, nil, nil, nil},
+}
+
+config :reversi_engine, :board_2, value: {
+  {nil, nil, nil, nil, nil, nil, nil, nil},
+  {nil, nil, nil, nil, nil, nil, nil, nil},
+  {nil, nil, nil, nil, nil, nil, nil, nil},
+  {nil, nil, nil, "B", "W", nil, nil, nil},
+  {nil, nil, nil, "W", "B", nil, nil, nil},
+  {nil, nil, nil, nil, nil, nil, nil, nil},
+  {nil, nil, nil, nil, nil, nil, nil, nil},
+  {nil, nil, nil, nil, nil, nil, nil, nil}
+}
+
+config :reversi_engine, :board_3, value: {
+  {nil, nil, nil, nil, nil, nil, nil, nil},
+  {nil, nil, nil, nil, nil, nil, nil, nil},
+  {nil, nil, nil, nil, nil, nil, nil, nil},
+  {nil, nil, nil, "B", "W", nil, nil, nil},
+  {nil, nil, nil, "W", "B", nil, nil, nil},
+  {nil, nil, "B", nil, nil, "W", nil, nil},
+  {nil, nil, nil, nil, nil, nil, nil, nil},
+  {nil, nil, nil, nil, nil, nil, nil, nil}
+}
+
+config :reversi_engine, :board_4, value: {
+  {nil, nil, nil, nil, nil, nil, nil, nil},
+  {nil, nil, nil, nil, nil, nil, nil, nil},
+  {nil, nil, "W", nil, nil, "B", nil, nil},
+  {nil, nil, nil, "B", "W", nil, nil, nil},
+  {nil, nil, nil, "W", "B", nil, nil, nil},
+  {nil, nil, nil, nil, nil, nil, nil, nil},
+  {nil, nil, nil, nil, nil, nil, nil, nil},
+  {nil, nil, nil, nil, nil, nil, nil, nil}
+}
